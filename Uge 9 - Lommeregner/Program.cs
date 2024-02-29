bf0441209bfe6adca4cd110fbeff5f7feb9ff083
Jesper Helstrup <jehe63306@edu.ucl.dk>
2024-02-29 14:42:54 +0100

    Add project files.

---
+++ Uge 9 - Lommeregner/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Uge_9___Lommeregner
+{
+    internal class ProgramJonathan
+    {
+        static void MainJonathan(string[] args)
+        {
+            Calculator Lommeregner = new Calculator();
+            //Console.WriteLine(Lommeregner.Add(2, 2));
+            //Console.WriteLine(Lommeregner.Subtract(10, 5));
+            //Console.WriteLine(Lommeregner.Divide(10, 3));
+            //Console.WriteLine(Lommeregner.Multiply(10, 2));
+
+            Console.WriteLine("Vælg i menu hvilken udregning du vil vælge");
+            Console.WriteLine("Tast 1 for Plus \nTast 2 for Minus \nTast 3 for Divider \nTast 4 for Multiply eller \ntast q for at Quit");
+
+            string userInput = Console.ReadLine();               
+
+            do
+            {
+                if (userInput == "1")
+                {
+                    Console.WriteLine("Du har valgt Plus, Indtast 2 værdier");
+                    Console.WriteLine(
+                        Lommeregner.Add(
+                            int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())
+                            ));
+                }
+                else if (userInput == "2")
+                {
+                    Console.WriteLine("Du har valgt Minus, Indtast 2 værdier");
+                    Console.WriteLine(
+                        Lommeregner.Subtract(
+                            int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())
+                            ));
+                }
+                else if (userInput == "3")
+                {
+                    Console.WriteLine("Du har valgt Divider, Indtast 2 værdier");
+                    Console.WriteLine(
+                        Lommeregner.Divide(
+                            int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())
+                            ));
+                }
+                else if (userInput == "4")
+                {
+                    Console.WriteLine("Du har valgt Multiply, Indtast 2 værdier");
+                    Console.WriteLine(
+                        Lommeregner.Multiply(
+                            int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())
+                            ));
+                }              
+            }
+            while (
+            
+            userInput != "q");
+            // o	Det skal være muligt at benytte alle metoderne på skift.
+            //
+            //      Loopen skal køre medmindre man skriver q
+            //
+            //
+
+
+        }
+    }
+}
---
+++ Uge 9 - Lommeregner/ProgramJesper.cs
@@ -0,0 +1,59 @@
+﻿namespace Uge_9___Lommeregner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator lommeregner = new Calculator();
+            int userinput = 0;
+
+                Console.Clear();
+                Console.Write("Indtast den første værdi:");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Indtast den anden værdi:");
+                int b = int.Parse(Console.ReadLine());
+                Console.WriteLine("Vælg hvilken metode du vil anvende:");
+                Console.WriteLine("1. add");
+                Console.WriteLine("2. subtract");
+                Console.WriteLine("3. multiply");
+                Console.WriteLine("4. divide");
+                Console.WriteLine("Eller skriv 5 for at afslutte");
+                userinput = int.Parse(Console.ReadLine());
+            
+            while (userinput == 1)
+            {
+                Console.WriteLine(lommeregner.Add(a, b));
+                break;
+            }
+            while (userinput == 2)
+            {
+                Console.WriteLine(lommeregner.Subtract(a, b));
+                break;
+            }
+            while (userinput == 3)
+            {
+                Console.WriteLine(lommeregner.Multiply(a, b));
+                break;
+            }
+            while (userinput == 4)
+            {
+                Console.WriteLine(lommeregner.Divide(a, b));
+                break;
+            }
+            while (userinput == 5)
+            {
+                Environment.Exit(0);
+                break;
+            }
+            while (userinput != 1 || userinput != 2 || userinput != 3 || userinput != 4 || userinput != 5)
+            {
+                Console.WriteLine("?");
+                break;
+            }
+            
+
+
+
+        }
+    }
+}
